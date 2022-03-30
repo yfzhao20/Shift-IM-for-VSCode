6172ad00b2021636880656fd3969ecc3e4fd925b
yfzhao <80528249+yfzhao20@users.noreply.github.com>
2022-03-30 20:20:56 +0800

    update 0.0.3 version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,8 @@
 ## [0.0.2]
 
 - Initial release
+
+## [0.0.3]
+
+- Change TextMate extension to `yfzhao.hscopes-booster` (**HyperScopes Booster**)
+    - Now you can uninstall `draivin.hscopes` (**HyperScopes**) extension if no extension depends on that.
--- src/hscopes.js
+++ src/hscopes.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const vscode = require('vscode');
-const hscopes = vscode.extensions.getExtension('draivin.hscopes');
+const hscopes = vscode.extensions.getExtension('yfzhao.hscopes-booster');
 
 function getScope(document, position) {
     if (!hscopes || !document || !position) {
